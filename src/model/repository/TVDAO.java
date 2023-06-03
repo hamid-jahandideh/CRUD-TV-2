2529d71b3a5eb1c4afe64541bcfb5b412dc2de84
eta2j <hamid.2jahandideh@gmail.com>
2023-06-03 12:19:05 +0330

    build save in dao

---
+++ src/model/repository/TVDAO.java
@@ -0,0 +1,8 @@
+package model.repository;
+
+import model.entity.TV;
+
+public class TVDAO implements TVDAOIX {
+    public void save(TV tv){
+    }
+}
---
+++ src/model/repository/TVDAOIX.java
@@ -0,0 +1,7 @@
+package model.repository;
+
+import model.entity.TV;
+
+public interface TVDAOIX {
+    public void save(TV tv);
+}
