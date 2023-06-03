8296fd6b148dcd7476bc749f8a459b76fa395d78
eta2j <hamid.2jahandideh@gmail.com>
2023-06-03 12:23:33 +0330

    service ro az bala be negah kon

---
+++ src/model/service/TVService.java
@@ -0,0 +1,10 @@
+package model.service;
+
+import model.entity.TV;
+
+public class TVService implements TVServiceIX{
+    @Override
+    public void save(TV tv) {
+
+    }
+}
---
+++ src/model/service/TVServiceIX.java
@@ -0,0 +1,7 @@
+package model.service;
+
+import model.entity.TV;
+
+public interface TVServiceIX {
+    public void save(TV tv);
+}
