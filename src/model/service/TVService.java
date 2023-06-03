e68745e24abc9523da66f47ca36882e48c352359
eta2j <hamid.2jahandideh@gmail.com>
2023-06-03 12:24:16 +0330

    hala dargire joziat sho

--- src/model/service/TVService.java
+++ src/model/service/TVService.java
@@ -1,10 +1,13 @@
 package model.service;
 
 import model.entity.TV;
+import model.repository.TVDAO;
+import model.repository.TVDAOIX;
 
 public class TVService implements TVServiceIX{
+    private TVDAOIX tvdaoix = new TVDAO();
     @Override
     public void save(TV tv) {
-
+        tvdaoix.save(tv);
     }
 }
