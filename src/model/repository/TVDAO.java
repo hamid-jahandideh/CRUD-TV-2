ac9ab59ec73537f3eff30deaafb2080c77e981ef
eta2j <hamid.2jahandideh@gmail.com>
2023-06-03 12:22:13 +0330

    save IMPLEMENTATION ( bayad yad begiri aval az bala negah koni bad biay dargire joziat beshi... man too commit ghabl ba khodam goftam ke method save vojood dare. in save gharare voroodi TV begire. amma kar nadashtam ke chejoori mikhad save ro anjam bede( be badane method kar nadaram) bad too commit bad oomadam joz'iat roo neveshtam

--- src/model/repository/TVDAO.java
+++ src/model/repository/TVDAO.java
@@ -2,7 +2,12 @@ package model.repository;
 
 import model.entity.TV;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TVDAO implements TVDAOIX {
+    private static List<TV> tvList = new ArrayList<>();
     public void save(TV tv){
+        tvList.add(tv);
     }
 }
